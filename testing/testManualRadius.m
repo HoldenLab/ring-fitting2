02b896702689bc71855835e52cf7dfe08a4027ca
seamusholden <seamus.holden@ncl.ac.uk>
2019-02-19 17:53:17 +0000

    Test case for the manual radius setting
    With the GFP cyto data

---
+++ testing/testManualRadius.m
@@ -0,0 +1,10 @@
+%TEST for manual radius setting
+fname = '190214_1mW_vert_1_MMStack_Pos0.ome_denoise_reg_ring1.tif'
+
+pixSz = 65; % [nm]
+psfFWHM = 300; % [nm]Check with beads
+
+%option to fix the radius
+manualRadius=350;
+batchAnalyseRing(fname2,pixSz,'SaveRawKymograph',true,'Radius',manualRadius)%I dont think bg subtracted data will necessarily make lots of sense with the cyto gfp data?
+
