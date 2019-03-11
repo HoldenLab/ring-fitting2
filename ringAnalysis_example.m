71c9800fc1052d3447fd79a60f9c367f62958c3e
kevinwhitley <kevin.whitley@newcastle.ac.uk>
2019-03-11 10:06:17 +0000

    Update ringAnalysis_example.m

--- ringAnalysis_example.m
+++ ringAnalysis_example.m
@@ -1,5 +1,5 @@
 fileFilter='*.tif';
-pixelSize=64;
+pixSz=65;
 %by default, fits with fixed radius based on average image
 batchAnalyseRing(fileFilter,pixSz)
 %alternatively, fit with a free radius to allow for constriction
