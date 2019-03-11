c2928c2f76b2e870816968f82faecdb2442d357a
seamusholden <seamus.holden@ncl.ac.uk>
2019-03-11 12:01:07 +0000

    first attempt at kymo/tracking overlay work

--- getTrackAngle.m
+++ getTrackAngle.m
@@ -1,31 +1,33 @@
-function [trackCirc,theta,rkymo,rtrack]= getTrackAngle(track,circleFitData)
+function [trackCirc,theta,rkymo,rtrack]= getTrackAngle(track,circleFitData,pixSz)
 %circleFitData is the cell of fit results (1 per frame) for each kymograph
 %trackCirc = [track,theta,rtrack,rkymo];
 
+PIXOFFSET=-1;%1 pixel shift between trackmate and matlab
 
 x0=[]; 
 y0=[];
 r0=[];
 nFr = numel(circleFitData);
+circCentreOffset= +1/2;
 for ii = 1:nFr
-    x0(ii) = circleFitData{ii}.z(1);
-    y0(ii) = circleFitData{ii}.z(2);
+    x0(ii) = circleFitData{ii}.z(1)+circCentreOffset;
+    y0(ii) = circleFitData{ii}.z(2)+circCentreOffset;
     r0(ii) = circleFitData{ii}.r;
 end
 
 nPt = size(track,1);
 %trackmate zero indexes frames, matlab uses 1 index
 fr = track(:,1)+1;
-x = track(:,2);
-y = track(:,3);
+xPix = track(:,2)/pixSz;
+yPix = track(:,3)/pixSz;
 
-rkymo= zeros(size(x));
-dx= zeros(size(x));
-dy= zeros(size(x));
+rkymo= zeros(size(xPix));
+dx= zeros(size(xPix));
+dy= zeros(size(xPix));
 for ii = 1:nPt
     frCur = fr(ii);
-    dx(ii,1) = x(ii) - x0(frCur);
-    dy(ii,1) = y(ii) - y0(frCur);
+    dx(ii,1) = xPix(ii) - x0(frCur);
+    dy(ii,1) = yPix(ii) - y0(frCur);
     rkymo(ii,1) = r0(frCur);
 end
 
