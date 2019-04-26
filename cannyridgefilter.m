f5d5b0bfa33508ccc37042d3ecd0b7ff777fa2e0
seamusholden <seamus.holden@ncl.ac.uk>
2019-04-26 13:01:03 +0100

    canny ridge filter basically works

--- cannyridgefilter.m
+++ cannyridgefilter.m
@@ -1,6 +1,7 @@
-function [ridge ridge_nms ] = cannyridgefilter(im,blurSigma,thresh)
-%function [ridge ridge_nms ] = cannyridgefilter(im,blurSigma,thresh)
+function [ridgeBW ridge nms nms_8con  ] = cannyridgefilter(im,blurSigma,thresh)
+%function [ridge nms ] = cannyridgefilter(im,blurSigma,thresh)
 %ridge filter = major eigenvalue of the image hessian
+%based on Canny_v0.m from matlab file exchange
 
 if ~exist('blurSigma','var')
     blurSigma = 1;
@@ -31,21 +32,45 @@ for ii = 1:numel(im)
     valley(ii) = max(e);
     ridge(ii) = -1*min(e);
 end
-
 %3.non-maximum suppression
 [nR nC] = size(im);
-ridge_nms = zeros(nR, nC);
+nms = zeros(nR, nC);
+nms_8con = zeros(nR, nC);
 for ii = 2:nR-1 %could make this better by padding
     for jj=2:nC-1
         if (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii+1,jj), ridge(ii-1,jj)])) || ...
-            (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii,jj+1), ridge(ii,jj-1)]))
-            %could do the 45 degree calculations too
-            ridge_nms(ii,jj) = ridge(ii,jj);
+            (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii,jj+1), ridge(ii,jj-1)])) 
+                nms(ii,jj) = ridge(ii,jj);
+                nms_8con(ii,jj) = ridge(ii,jj);
+            %do the 45 degree calculations too
+        elseif (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii+1,jj+1), ridge(ii-1,jj-1)])) || ...
+            (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii-1,jj+1), ridge(ii+1,jj-1)]))
+                nms_8con(ii,jj) = ridge(ii,jj);
         end
     end
 end
 
 
 %4. double threshold
-T_high = gra
+Totsu = graythresh(ridge);
+T_High =thresh(1) *Totsu;%or could implement absolute thresholds?
+T_Low = thresh(2)*Totsu;
+
 %5. Edge tracking by hysteresis
+
+T_res = zeros (nR, nC);
+
+for i = 1  : nR
+    for j = 1 : nC
+        if (nms_8con(i, j) < T_Low)
+            T_res(i, j) = 0;
+        elseif (nms_8con(i, j) > T_High)
+            T_res(i, j) = 1;
+        %Using 8-connected components
+        elseif ( nms_8con(i+1,j)>T_High || nms_8con(i-1,j)>T_High || nms_8con(i,j+1)>T_High || nms_8con(i,j-1)>T_High || nms_8con(i-1, j-1)>T_High || nms_8con(i-1, j+1)>T_High || nms_8con(i+1, j+1)>T_High || nms_8con(i+1, j-1)>T_High)
+            T_res(i,j) = 1;
+        end;
+    end;
+end;
+
+ridgeBW = logical(T_res.*255);
