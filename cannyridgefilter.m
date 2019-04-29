ddf9146c7aabcc67a790769fdef3c16b3dfdc94b
seamusholden <seamus.holden@ncl.ac.uk>
2019-04-29 11:07:13 +0100

    implemented horz connectivity on cannyridge

--- cannyridgefilter.m
+++ cannyridgefilter.m
@@ -1,13 +1,26 @@
-function [ridgeBW ridge nms nms_8con  ] = cannyridgefilter(im,blurSigma,thresh)
+function [ridgeBW ridge nms ] = cannyridgefilter(im,varargin)
 %function [ridge nms ] = cannyridgefilter(im,blurSigma,thresh)
 %ridge filter = major eigenvalue of the image hessian
 %based on Canny_v0.m from matlab file exchange
 
-if ~exist('blurSigma','var')
-    blurSigma = 1;
-end
-if ~exist('thresh','var')
-    thresh= [1,0.5];
+blurSigma = 1;
+thresh= [1,0.5];
+nms_con = 'horz';
+nargin = numel(varargin);
+ii = 1;
+while ii<=numel(varargin)
+    if strcmp(varargin{ii}, 'BlurSigma')
+        blurSigma=varargin{ii+1};
+        ii = ii+2;
+    elseif strcmp(varargin{ii}, 'Threshold')
+        thresh=varargin{ii+1};
+        ii = ii+2;
+    elseif strcmp(varargin{ii}, 'NMS-con')
+        nms_con=varargin{ii+1};
+        ii = ii+2;
+    else
+        ii = ii+1;
+    end
 end
 
 
@@ -37,17 +50,24 @@ ridge(ridge<0)=0;
 %3.non-maximum suppression
 [nR nC] = size(im);
 nms = zeros(nR, nC);
-nms_8con = zeros(nR, nC);
 for ii = 2:nR-1 %could make this better by padding
     for jj=2:nC-1
-        if (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii+1,jj), ridge(ii-1,jj)])) || ...
-            (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii,jj+1), ridge(ii,jj-1)])) 
+        if strcmp(nms_con,'horz')%this makes the most sense for kymographs
+            if (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii,jj+1), ridge(ii,jj-1)])) 
                 nms(ii,jj) = ridge(ii,jj);
-                nms_8con(ii,jj) = ridge(ii,jj);
-            %do the 45 degree calculations too
-        elseif (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii+1,jj+1), ridge(ii-1,jj-1)])) || ...
-            (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii-1,jj+1), ridge(ii+1,jj-1)]))
-                nms_8con(ii,jj) = ridge(ii,jj);
+            end
+        elseif strcmp(nms_con,'4-con')
+            if (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii+1,jj), ridge(ii-1,jj)])) || ...
+                (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii,jj+1), ridge(ii,jj-1)])) 
+                    nms(ii,jj) = ridge(ii,jj);
+            end
+        elseif strcmp(nms_con,'8-con')
+            if (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii+1,jj+1), ridge(ii-1,jj-1)])) || ...
+                (ridge(ii,jj) == max([ridge(ii,jj), ridge(ii-1,jj+1), ridge(ii+1,jj-1)]))
+                    nms(ii,jj) = ridge(ii,jj);
+            end
+        else
+            error('cannyridgefilter:unrecognizedNmsArg','Unregonized NMS connectivity argument');
         end
     end
 end
@@ -64,12 +84,12 @@ T_res = zeros (nR, nC);
 
 for i = 1  : nR
     for j = 1 : nC
-        if (nms_8con(i, j) < T_Low)
+        if (nms(i, j) < T_Low)
             T_res(i, j) = 0;
-        elseif (nms_8con(i, j) > T_High)
+        elseif (nms(i, j) > T_High)
             T_res(i, j) = 1;
-        %Using 8-connected components
-        elseif ( nms_8con(i+1,j)>T_High || nms_8con(i-1,j)>T_High || nms_8con(i,j+1)>T_High || nms_8con(i,j-1)>T_High || nms_8con(i-1, j-1)>T_High || nms_8con(i-1, j+1)>T_High || nms_8con(i+1, j+1)>T_High || nms_8con(i+1, j-1)>T_High)
+        %Using 8-conected components
+        elseif ( nms(i+1,j)>T_High || nms(i-1,j)>T_High || nms(i,j+1)>T_High || nms(i,j-1)>T_High || nms(i-1, j-1)>T_High || nms(i-1, j+1)>T_High || nms(i+1, j+1)>T_High || nms(i+1, j-1)>T_High)
             T_res(i,j) = 1;
         end;
     end;
