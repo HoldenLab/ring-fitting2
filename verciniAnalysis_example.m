c6824bd7bf1a0027682e93ca8299b7fd76922a1b
Seamus Holden <seamus.holden@ncl.ac.uk>
2021-03-17 11:32:28 +0000

    more tidying

--- verciniAnalysis_example.m
+++
@@ -1,8 +0,0 @@
-fileFilter='*.tif';
-pixSz=65;
-%by default, fits with fixed radius based on average image
-batchAnalyseRing(fileFilter,pixSz)
-%alternatively, fit with a free radius to allow for constriction
-%batchAnalyseRing(fileFilter,pixSz,'FixedRadiusFit',false)
-%optionally you can save the non-background subtracted kymograph
-%batchAnalyseRing(fileFilter,pixSz,'SaveRawKymograph',true)
