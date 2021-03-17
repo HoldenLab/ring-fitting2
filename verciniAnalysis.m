bd6fdae057ffcb6497fb5ac3ac6b931e8b62d2cf
Seamus Holden <seamus.holden@ncl.ac.uk>
2021-03-17 11:21:05 +0000

    rename core functions for clarity
    and start to clarify the documentation

--- verciniAnalysis.m
+++ verciniAnalysis.m
@@ -1,36 +1,8 @@
-function batchAnalyseRing(fileFilter,pixSz,varargin)
-% function batchAnalyseRing(fileFilter,pixSz,varargin)
-%   Performs background subtraction and kymograph fitting for vertically immobilized cells
-%   Kymographs are background subtracted ie zero should equal a genuine gap in the ring
+function verciniAnalysis(fileFilter,pixSz,varargin)
+% Performs circular kymograph analysis for VerCINI microscopy data of vertically immobilized bacteria
 % INPUTS:
 %   fileFilter: Search string for files to analyse, eg '*.tif'
 %   pixSz: Camera pixel size in nanometres
-% OPTIONAL INPUTS:
-%   'PsfFWHM', psfFWHM: Fitted PSF FWHM, nm. Determines PSF size used to blur the fitted ring. DEFAULT: 300 nm
-%   'PsfWidthRangeNm',psfWidthExtraNm: Wiggle room allowed on fitted PSF FHWM. Ie fitted PSF width can be within range psfFWHM +/- psfWidthExtraNm. DEFAULT: 50
-%   'CytoplasmBG-FWHM', cytoBgFWHM_nm: Initial guess for the FWHM of the large gaussian fitted to account for the defocussed cytoplasmic background. DEFAULT:1300
-%   'CytoplasmBG-FWHM-min', cytoBgFWHMmin_nm: Minimum for the FWHM of the large gaussian fitted to account for the defocussed cytoplasmic background. DEFAULT:800
-%   'CytoplasmBG-FWHM-max', cytoBgFWHMmax_nm: Maximum for the FWHM of the large gaussian fitted to account for the defocussed cytoplasmic background. DEFAULT:Inf
-%   'CytoplasmOnlyFit' Dont fit a ring - useful for cytoGFP - force the ring amplitude to zero for cyto only fit
-% NOTE: A second defocussed Gaussian is also fitted, with min width cytoBgFWHMmin_nm, and max width=Inf because a single gaussian does not fit well the cytoplasmic BG distribution.
-%   'RingRadius-max', radMax_nm: Maximum fitted ring radius. Default should hold well for WT or even most mutant Bsubtilis but change if in a different organism. If you set it too large the fitting becomes unstable for small rings. DEFAULT: 600
-%   'ZeroPadKymograph', doZeroPadKymo: Add a zero row as the last row of the kymograph so that ImageJ plotting defaults to the correct contrast. DEFAULT: true
-%   'FixedRadiusFit', true/false: Fix the ring radius and shape parameters to the average ring parameters. Note the ring centroid can still shift to allow for small drifts. 
-%     Useful for cells that dont constrict within timeframe of imaging. If the cells constrict you need to turn this off. 
-%     FITPARAVG is the result of a prior fit to an averaged ring, used to fix the positions. DEFAULT: true
-%   'FixedPositionFit', true/false: Fix the ring position to the average ring position. This is important for fitting sparse data, esp single molecule, where the ring position is not well constrained.
-%     otherwise the ring position will jump around, which is bad.  DEFAULT: false 
-%   'LineProfileWidth': LineWidth: perpendicular distance over which to integrate line profile signal to improve SNR. DEFAULT:pixSz , ie 1 pixel
-%   'NumKymoRepeats', nKymoWrap: Number of times to plot the kymograph side-by-side in the kymoWrap file. DEFAULT: 2
-%   'SaveRawKymograph', true/false: Save a non-background subtracted kymograph as well. DEFAULT, false
-%   'SaveFitPNG', true/ false: Save a png of the average image overlaid with the (first) fitted circle
-%   'SetManualRadius', manualRadiusNm: Sets the septum radius to fixed value manualRadiusNm
-%   'SetManualRadius', [manualRadiusNmMin,manualRadiusNmMax]: limits the septum radius to range defined by 2 element vector. Initial guess is average of the min and max Radius
-%   'FitMaxIP', true/false: Use maximum intensity projection instead of average for the fixed radius/ position fitting
-%   'HoughCircleGuess',true/false:Uses hough circle finding estimator for the initial guess. In general seems more robust than the simple binary estimator
-%
-% NOTE: If the background subtration fails for some frames - slow fitting, bright bands in the kymographs - this is usually because 'FixedRadiusFit' is set to true, but the radius is changing  - try changing 'FixedRadiusFit' to false. If the radius is changign the FixedRadiusFit gives bad results as the average radius is not a good match for all frames
-%
 % OUTPUTS:
 %   Files, in subdirectory analysed:
 %       <filename>_bgsub.tif: Background subtracted ring 
@@ -38,16 +10,43 @@ function batchAnalyseRing(fileFilter,pixSz,varargin)
 %       <filename>_kymoWrap.tif: Kymograph, repeated nKymoWrap times
 %       <filename>_fitData.mat: Fit results
 %       <filename>_diamInfo.txt: Ring diameter, each frame
+% OPTIONAL INPUTS (commonly used):
+%   'FixedRadiusFit', true/false: Fix the ring radius and shape parameters to the average ring parameters. Note the ring centroid can still shift to allow for small drifts. 
+%     Useful for cells that dont constrict within timeframe of imaging. If the cells constrict you need to turn this off. 
+%     FITPARAVG is the result of a prior fit to an averaged ring, used to fix the positions. DEFAULT: true
+%   'FixedPositionFit', true/false (default:false): Fix the ring position to the average ring position. This is important for fitting sparse data, esp single molecule, where the ring position is not well constrained. Otherwise the ring position will jump around, which is bad.  
+%   'NumKymoRepeats', nKymoWrap (default:2): Number of times to plot the kymograph side-by-side in the kymoWrap file.
+%   'SaveRawKymograph', true/false (default:false): Save a non-background subtracted kymograph as well. 
+%   'SaveFitPNG', true/ false (default:true): Save a png of the average image overlaid with the (first) fitted circle
+%   'ZeroPadKymograph', true/ false (default:true): Add a zero row as the last row of the kymograph so that ImageJ plotting defaults to the correct contrast. DEFAULT: true
+% OPTIONAL INPUTS (advanced):
+%   'CytoplasmBG-FWHM', cytoBgFWHM_nm: Initial guess for the FWHM of the large gaussian fitted to account for the defocussed cytoplasmic background. DEFAULT:1300
+%   'CytoplasmBG-FWHM-min', cytoBgFWHMmin_nm: Minimum for the FWHM of the large gaussian fitted to account for the defocussed cytoplasmic background. DEFAULT:800
+%   'CytoplasmBG-FWHM-max', cytoBgFWHMmax_nm: Maximum for the FWHM of the large gaussian fitted to account for the defocussed cytoplasmic background. DEFAULT:Inf
+%       NOTE: A second defocussed Gaussian is also fitted, with min width cytoBgFWHMmin_nm, and max width=Inf because a single gaussian does not fit well the cytoplasmic BG distribution.
+%   'CytoplasmOnlyFit' Dont fit a ring - useful for calibration/ testing analysis of cytoplasmic GFP samples - force the ring amplitude to zero for cyto only fit
+%   'FitMaxIP', true/false (default:false): Use maximum intensity projection instead of average for the fixed radius/ position fitting
+%   'LineProfileWidth', LineWidth (default:pixSz , ie 1 pixel): perpendicular distance over which to integrate line profile signal to improve SNR. 
+%   'PsfFWHM', psfFWHM (default:300 nm): Fitted PSF FWHM, nm. Determines PSF size used to blur the fitted ring. 
+%   'PsfWidthRangeNm',psfWidthExtraNm: Wiggle room allowed on fitted PSF FHWM. Ie fitted PSF width can be within range psfFWHM +/- psfWidthExtraNm. DEFAULT: 50
+%   'RingRadius-max', radMax_nm (default: 600): Maximum fitted ring radius. Default should hold well for most B subtilis or E coli samples but might need to change if in a different organism. If you set it too large the fitting becomes unstable for small radius septa/ cells. 
+%   'SetManualRadius', manualRadiusNm: Sets the septum radius to fixed value manualRadiusNm
+%   'SetManualRadius', [manualRadiusNmMin,manualRadiusNmMax]: limits the septum radius to range defined by 2 element vector. Initial guess is average of the min and max Radius
+% OPTIONAL INPUTS (developer/debug):
+%   'HoughCircleGuess',true/false: (Experimental) Uses hough circle finding estimator for the initial guess. From initial testing seems more robust than the simple binary estimator. 
+%
+% NOTE: If the background subtration fails for some frames - slow fitting, bright bands in the kymographs - this is usually because 'FixedRadiusFit' is set to true, but the radius is changing  - try changing 'FixedRadiusFit' to false. If the radius is changign the FixedRadiusFit gives bad results as the average radius is not a good match for all frames
+%
 %
 % EXAMPLES:
 %   
 %   By default, fits with fixed radius based on average image
-%   >> batchAnalyseRing('*.tif',pixSz)
+%   >> verciniAnalysis('*.tif',pixSz)
 %   Alternatively, fit with a free radius to allow for constriction
-%   >> batchAnalyseRing('*.tif',pixSz,'FixedRadiusFit',false)
+%   >> verciniAnalysis('*.tif',pixSz,'FixedRadiusFit',false)
 %   Optionally you can save the non-background subtracted kymograph
-%   >> batchAnalyseRing('*.tif',pixSz,`'SaveRawKymograph',true)
-%TODO fix the diameter file bug
+%   >> verciniAnalysis('*.tif',pixSz,`'SaveRawKymograph',true)
+
 
 f = dir(fileFilter);
 nF= numel(f);
