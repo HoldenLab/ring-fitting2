77c0416cbe942aab616de4e6c4e4a7100f1b1481
Seamus Holden <seamus.holden@ncl.ac.uk>
2021-03-17 13:45:06 +0000

    Update manualVerciniAnalysis.m
    add documentation

--- manualVerciniAnalysis.m
+++ manualVerciniAnalysis.m
@@ -1,5 +1,32 @@
-function batchManualKymo(fileFilter,pixSz)
-NKYMOWRAP=3;
+function manualVerciniAnalysis(fileFilter,pixSz,nKymoWrap)
+% Performs manual circular kymograph analysis for VerCINI microscopy data of vertically immobilized bacteria
+% Kymograph is calculated along a circular line profile manually selected for each image stack, based on the maximum
+% intensity projection of the stack
+% This is mostly used for sparse single molecule datasets if automated VerCINI analysis has failed
+% Automatically analyses all files matching "fileFilter" string
+%
+% NOTE: By default, TWO copies of the kymograph are plotted side by side, ie wrapping around the cell circumference twice, 
+% 0->720degrees. This is so that dynamics at the 360degree->0degree transition can still be identified
+% INPUTS:
+%   fileFilter: Search string for files to analyse, eg '*.tif'
+%       Input image stacks should be cropped movies of septum/ circumferentially localized protein dynamics.
+%       Cell should not overlap image edges.
+%   pixSz: Camera pixel size in nanometres
+% OUTPUTS:
+%   Files, in subdirectory analysed:
+%       <filename>_kymo.tif: Kymograph
+%       <filename>_kymoWrap.tif: Kymograph, repeated nKymoWrap times
+%       <filename>_fitData.mat: Parameters defining the circle used for kymograph analysis.
+%       <filename>_diamInfo.txt: Ring diameter, each frame if constricting, otherwise only defined for first frame (same for the rest)
+%
+%
+% OPTIONAL INPUTS 
+%   nKymoWrap (default:2): Number of times to plot the kymograph side-by-side in the kymoWrap file.
+% 
+%TODO SAVE THE DIAMETER
+if ~exist(nKymoWrap)
+    nKymoWrap=2;
+end
 
 f = dir(fileFilter);
 nF= numel(f);
@@ -10,7 +37,7 @@ for ii = 1:nF
     folder=f(ii).folder;
     fname = fullfile(folder,file)
     try 
-        manualCircleAndKymo(fname,pixSz,NKYMOWRAP);
+        manualCircleAndKymo(fname,pixSz,nKymoWrap);
     catch ME
         display(getReport(ME));
     end
